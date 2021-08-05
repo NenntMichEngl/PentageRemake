6e22365320d1412a360ac7bdad6798a0ddd67e69
NenntMichEngl <59359544+NenntMichEngl@users.noreply.github.com>
2021-08-05 15:21:15 +0200

    Change

---
+++ PentagoRemake/Assets/PointSwitcher.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointSwitcher : MonoBehaviour
+{
+    public bool empty = true;
+    public bool white;
+    GameManager man
+    void Start()
+        manager = GetC
+    }
+    void OnMouseDown()
+    {
+    }
+}
